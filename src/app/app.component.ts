eadae1a1bd14ef26cfe4faa386897f092b16174d
Gaurish <megaurishrane@gmail.com>
2020-05-01 03:01:31 +0530

    control auth changs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Component({
@@ -17,11 +17,17 @@ export class AppComponent {
   showSignup: boolean = false;
   showDashboard: boolean = false;
 
+  loadingStatus: boolean = false;
+  loadingData: boolean = false;
+
   baseUrl: string = 'http://localhost:5555/iconnect-server/';
 
   title = 'iconnect-frontend';
   loginForm: FormGroup;
   signUpForm: FormGroup;
+
+  token: string = '';
+  userDetails: any[] = [];
 
   constructor(public fb: FormBuilder, public http: HttpClient) {
   }
@@ -54,17 +60,22 @@ export class AppComponent {
 
   loginSubmit(event) {
     if (this.loginForm.valid) {
-      console.log('logins', this.loginForm.value);
+      this.loadingStatus = true;
       this.http.post(this.baseUrl + 'users/login', this.loginForm.value, {})
         .subscribe((data: any) => {
+          this.loadingStatus = false;
           if (data.status) {
-            alert(data.message);
+            this.token = data.token;
             this.showLogin = this.showSignup = false;
             this.showDashboard = true;
+            this.getUserDetails();
+            this.loginForm.reset();
+            alert(data.message);
           } else {
             alert(data.reason);
           }
         }, (error: any) => {
+          this.loadingStatus = false;
           console.error('error =>', error);
         });
     } else {
@@ -74,22 +85,45 @@ export class AppComponent {
 
   signUpSubmit(event) {
     if (this.signUpForm.valid) {
-      console.log('logins', this.signUpForm.value);
+
+      this.loadingStatus = true;
 
       this.http.post(this.baseUrl + 'users/registration', this.signUpForm.value, {})
         .subscribe((data: any) => {
+          this.loadingStatus = false;
           if (data.status) {
+            this.signUpForm.reset();
             alert(data.message);
           } else {
             alert(data.reason);
           }
         }, (error: any) => {
+          this.loadingStatus = false;
           console.error('error =>', error);
         });
 
     } else {
       alert('Please enter required details.');
     }
+  }
+
+  getUserDetails() {
+
+    this.loadingData = true;
+    this.http.get(this.baseUrl + 'users/userDetails', {
+      headers: {
+        'authorization': 'Bearer ' + this.token
+      }
+    }).subscribe((data: any) => {
+      this.loadingData = false;
+      if (data.status) {
+        this.userDetails = data.data;
+      } else {
+        alert('Oops can not get your data.');
+      }
+    }, (error: any) => {
+      this.loadingData = false;
+    });
   }
 
   logout() {
